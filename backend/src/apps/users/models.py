919e26331fd2e41b7986fa604b5d7905304a895d
o4codes <oforkansi.shadrach@gmail.com>
2023-04-28 13:28:06 +0100

    modified libs models

---
+++ backend/src/apps/users/models.py
@@ -0,0 +1,1 @@
+from src.libs import DbModel
--- backend/src/libs/models.py
+++ backend/src/libs/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from enum import Enum
 from typing import TYPE_CHECKING, Optional, Union
 
+import orjson
 import pytz
 from bson import ObjectId
 from fastapi.encoders import jsonable_encoder
@@ -13,18 +14,56 @@ if TYPE_CHECKING:
     from pydantic.typing import AbstractSetIntStr, DictStrAny, MappingIntStrAny
 
 
+def orjson_dumps(v, *, default):
+    """rjson.dumps returns bytes, to match standard json.dumps we need to decode
+    """
+    return orjson.dumps(v, default=default).decode()
+
 class DbModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     created_at: datetime = Field(default=datetime.now().replace(tzinfo=pytz.utc))
     updated_at: Optional[datetime]
 
     class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str, datetime: lambda v: v.isoformat()}
+    
+    def serializable_dict(
+        self, 
+        include: DictStrAny = None, 
+        exclude: DictStrAny = None,
+        by_alias: bool = True,
+        exclude_unset: bool = False, 
+        exclude_defaults: bool = False,
+        exclude_none: bool = None
+    ):
+        """Return a dict which contains only serializable fields."""
+        return jsonable_encoder(
+            self, 
+            include=include, 
+            exclude=exclude, 
+            by_alias=by_alias, 
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none
+        )
 
 
-class PaginationModel(BaseModel):
+class ResponseStatus(str, Enum):
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
+
+
+class DefaultResponse(BaseModel):
+    status: ResponseStatus
+    message: str
+    data: Union[list, dict, BaseModel]
+
+
+class PaginationModel(DefaultResponse):
     total_count: int
     page: int
     size: int
@@ -46,14 +85,3 @@ class PaginationModel(BaseModel):
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str, datetime: lambda v: v.isoformat()}
 
-
-class ErrorModelStatus(str, Enum):
-    SUCCESS = "success"
-    FAILED = "failed"
-
-
-class ErrorModel(BaseModel):
-    status: ErrorModelStatus
-    message: str
-    details: Union[str, list, dict]
-    timestamp: float = datetime.now().timestamp()
