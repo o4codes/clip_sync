cadd5356e2d2776e028345d0ca943958368fbe00
o4codes <oforkansi.shadrach@gmail.com>
2023-04-28 14:37:40 +0100

    reformatted backend

--- backend/src/apps/users/repository.py
+++ backend/src/apps/users/repository.py
@@ -5,7 +5,7 @@ from . import models, constants
 class DeviceRepository(BaseRepository):
     model_klass = models.DeviceModel
     collection_name = constants.DEVICE_DB_COLLECTION_NAME
-    
+
 
 class UserRepository(BaseRepository):
     model_klass = models.UserModel
