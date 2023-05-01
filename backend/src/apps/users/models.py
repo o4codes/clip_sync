bb7d43d6ada4446b8d73ca552ea4049680a5e400
o4codes <oforkansi.shadrach@gmail.com>
2023-05-01 19:24:48 +0100

    modified device model

--- backend/src/apps/users/models.py
+++ backend/src/apps/users/models.py
@@ -1,19 +1,21 @@
 from typing import Optional, Any
 
-from pydantic import EmailStr
+from pydantic import EmailStr, BaseModel
 
 from src.libs import DbModel, PyObjectId
 
 
 class DeviceModel(DbModel):
     """
-    Holds information about device being used to share clips
     A device may either have or not have a user.
     Such cases means that the device owner doesn't have an account.
     """
 
-    device_name: str
-    username: str
+    operating_system: Optional[str] = None
+    browser: Optional[str] = None
+    device_family: Optional[str] = None
+    device_model: Optional[str] = None
+    device_brand: Optional[str] = None
     user_id: Optional[PyObjectId] = None
 
 
