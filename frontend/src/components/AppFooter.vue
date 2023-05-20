0b50ccbe98600b4820f17b986d2037c8ff0c4601
o4codes <oforkansi.shadrach@gmail.com>
2023-05-20 15:32:48 +0100

    modified frontend structure

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,9 +1,13 @@
 <script>
-
 export default {
 
 }
 </script>
 
 <template>
+    <p>THis is the footer</p>
 </template>
+    
+<style scoped>
+
+</style>
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+import { RefreshIcon } from "@vue-hero-icons/solid";
+
+export default {
+  
+}
+
+</script>
+
+<template>
+  <main>
+    <div>
+      <RefreshIcon />
+      <p>Clip-Sync</p>
+    </div>
+    <div>
+      <p>Sign Up</p>
+      <p>Log In</p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>>
