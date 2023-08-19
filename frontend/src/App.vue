117924a8cc9c92fa0196219e8b8f1eb83720ac76
o4codes <oforkansi.shadrach@gmail.com>
2023-08-19 21:38:05 +0100

    added typescript

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,27 @@
-<script>
-import { RouterLink, RouterView } from "vue-router";
-import AppHeader from "./components/AppHeader.vue";
-import AppFooter from "./components/AppFooter.vue";
-
-export default {
-  components: {
-    AppFooter, AppHeader
-  },
-}
-
-</script>
-
 <template>
-  <div id="app" class="flex flex-col">
-    <AppHeader />
-    <RouterView />
-    <AppFooter />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<style scoped>
-  /* #app {
-    background-image: url('./assets/bg.webp');
-  } */
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
